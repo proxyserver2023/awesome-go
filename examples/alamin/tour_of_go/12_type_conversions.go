a5074b57c20dbcbe964333c5c91924bc913d6906
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-10-12 17:02:51 +0600

    :beers: loop done

--- examples/alamin/tour_of_go/12_type_conversions.go
+++ examples/alamin/tour_of_go/12_type_conversions.go
@@ -1,14 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func main() {
-	x, y := 3, 4
-	f := math.Sqrt(float64(x*x + y*y))
-	z := uint(f)
-	fmt.Printf("%T\n", z)
-	fmt.Println(x, y, z)
-}
+//
+// func main() {
+// 	x, y := 3, 4
+// 	f := math.Sqrt(float64(x*x + y*y))
+// 	z := uint(f)
+// 	fmt.Printf("%T\n", z)
+// 	fmt.Println(x, y, z)
+// }
