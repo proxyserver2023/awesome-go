2aeb22dfbcd2d1f700756f724751952011cce7c6
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-13 08:12:05 +0600

    :beers: concurrency init

--- main.go
+++ main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"time"
 )
 
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
 func main() {
-	var x float64 = 3.4
-
-	t := reflect.TypeOf(x)
-	v := reflect.TypeOf(x)
-
-	fmt.Println("type: ", t)
-	fmt.Println("value: ", v)
-	fmt.Println("kind: ", v.Kind() == reflect.Float64)
-
-	// Pass by value
-	// v := reflect.ValueOf(x)
-	// v.SetFloat(7.1)
-
-	// Pass by reference
-	p := reflect.ValueOf(&x)
-	vv := p.Elem()
-	vv.SetFloat(7.1)
+	go say("world")
+	say("Hello")
 }
