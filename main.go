3006098fc4bdbfaa8c124dc7d237fb37acd51411
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-13 08:04:05 +0600

    :beers: reflection setting value vs reference

--- main.go
+++ main.go
@@ -14,4 +14,13 @@ func main() {
 	fmt.Println("type: ", t)
 	fmt.Println("value: ", v)
 	fmt.Println("kind: ", v.Kind() == reflect.Float64)
+
+	// Pass by value
+	// v := reflect.ValueOf(x)
+	// v.SetFloat(7.1)
+
+	// Pass by reference
+	p := reflect.ValueOf(&x)
+	vv := p.Elem()
+	vv.SetFloat(7.1)
 }
