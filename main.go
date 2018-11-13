69b62588207c6748d60f0db8900b2e826a274f68
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-13 08:28:19 +0600

    :beers: channel blocking vs non-blocking

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-)
+import "fmt"
 
-func say(s string, ii time.Duration) {
-	for i := 0; i < 5; i++ {
-		runtime.Gosched()
-		time.Sleep(ii * time.Millisecond)
-		fmt.Println(i, s)
+func sum(a []int, c chan int) {
+	total := 0
+	for _, v := range a {
+		total += v
 	}
+	c <- total // send total to c
 }
 
 func main() {
-	runtime.GOMAXPROCS(4)
-	go say("world", 1000)
-	say("Hello", 1000)
+	// input
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2, -3, -4, -5, -6, -7, -8, -9}
+
+	// init channel
+	c := make(chan int)
+
+	// asking goroutines for using channel to send the total
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+
+	// receiving the data from channel
+	x, y := <-c, <-c
+
+	// output
+	fmt.Println(x, y, x+y)
 }
