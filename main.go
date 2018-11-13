2dd2d98cacb11fedd25a6a05663cebbc833a1945
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-13 07:58:07 +0600

    :beers: reflection

--- main.go
+++ main.go
@@ -2,39 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"reflect"
 )
 
-type Element interface{}
-type List []Element
-
-type Person struct {
-	name string
-	age  int
-}
-
-func (p Person) String() string {
-	return "(name: " + p.name + " - age:    " + strconv.Itoa(p.age) + " years)"
-}
-
 func main() {
-	list := make(List, 3)
+	var x float64 = 3.4
 
-	list[0] = 1
-	list[1] = "2 as a string"
-	list[2] = Person{
-		name: "alamin",
-		age:  24,
-	}
+	t := reflect.TypeOf(x)
+	v := reflect.TypeOf(x)
 
-	for _, elem := range list {
-		if element, ok := elem.(int); ok {
-			fmt.Printf("%T=>%v\n", element, element)
-		} else if element, ok := elem.(string); ok {
-			fmt.Printf("%T=>%v\n", element, element)
-		} else if element, ok := elem.(Person); ok {
-			fmt.Printf("%T=>%v\n", element, element)
-		}
-	}
-
+	fmt.Println("type: ", t)
+	fmt.Println("value: ", v)
+	fmt.Println("kind: ", v.Kind() == reflect.Float64)
 }
