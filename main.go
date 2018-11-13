7647bb3a692375cf049630f660601996c7a8ba99
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-13 08:34:02 +0600

    :beers: Fatal Error: Deadlock

--- main.go
+++ main.go
@@ -2,28 +2,14 @@ package main
 
 import "fmt"
 
-func sum(a []int, c chan int) {
-	total := 0
-	for _, v := range a {
-		total += v
-	}
-	c <- total // send total to c
-}
-
 func main() {
-	// input
-	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -2, -3, -4, -5, -6, -7, -8, -9}
+	c := make(chan int, 10)
 
-	// init channel
-	c := make(chan int)
+	for i := 0; i < 10; i++ {
+		c <- i
+	}
 
-	// asking goroutines for using channel to send the total
-	go sum(a[:len(a)/2], c)
-	go sum(a[len(a)/2:], c)
-
-	// receiving the data from channel
-	x, y := <-c, <-c
-
-	// output
-	fmt.Println(x, y, x+y)
+	for i := 0; i < 11; i++ {
+		fmt.Println(<-c)
+	}
 }
