320401a7cb0aca70bbe4212686c20fb397eba941
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-23 10:18:24 +0600

    :beers: commiting dirty code
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// app "github.com/alamin-mahamud/awesome-go/source/simpleapi"
-// app "github.com/alamin-mahamud/awesome-go/source/gopl"
-// app "github.com/alamin-mahamud/awesome-go/source/golang"
-
-type T struct {
-	id int
-}
+	"github.com/alamin-mahamud/awesome-go/source/intensive/stringutil"
+)
 
 func main() {
-	//app.Run()
-	t1 := T{id: 1}
-	t2 := T{id: 2}
-	ts1 := []T{t1, t2}
-	ts2 := []*T{}
-	for _, t := range ts1 {
-		ts2 = append(ts2, &t)
-	}
-
-	for _, t := range ts2 {
-		fmt.Println((*t).id)
-	}
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
