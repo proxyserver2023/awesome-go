594f3f6ea601657cd0805baf5e55ad7590d66fbf
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-24 10:19:46 +0600

    :beers: working with solid and open closed
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/alamin-mahamud/awesome-go/source/intensive/stringutil"
-)
+// A is the base type in this package
+type A struct {
+	year int
+}
+
+// Greet greets us with a string
+func (a A) Greet() { fmt.Println("Hello", a.year) }
+
+// B is another type composed with A
+type B struct {
+	A
+}
+
+// Greet Overriding it
+func (b B) Greet() { fmt.Println("Welcome", b.year) }
 
 func main() {
-	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	var a A
+	a.year = 2016
+
+	var b B
+	b.year = 2018
+
+	a.Greet()
+	b.Greet()
 }
