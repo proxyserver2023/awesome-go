15722cc95a601fce9f937256ec2587a9fa6bcd9a
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-24 13:54:37 +0600

    amazed by the beauty of recursions
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- source/datastructures/list/list.go
+++ source/datastructures/list/list.go
@@ -50,3 +50,56 @@ func (l *list) Insert(val interface{}, pos uint) (PersistentList, error) {
 	return nl.Add(l.head), nil
 
 }
+
+func (l *list) Get(pos uint) (interface{}, bool) {
+	if pos == 0 {
+		return l.head, true
+	}
+	return l.tail.Get(pos - 1)
+}
+
+func (l *list) Remove(pos uint) (PersistentList, error) {
+	if pos == 0 {
+		nl, _ := l.Tail()
+		return nl, nil
+	}
+
+	nl, err := l.tail.Remove(pos - 1)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &list{l.head, nl}, nil
+}
+
+func (l *list) Find(pred func(interface{}) bool) (interface{}, bool) {
+	if pred(l.head) {
+		return l.head, true
+	}
+
+	return l.tail.Find(pred)
+}
+
+func (l *list) FindIndex(pred func(interface{}) bool) int {
+	curr := l
+	idx := 0
+	for {
+		if pred(l.head) {
+			return idx
+		}
+
+		tail, _ := curr.Tail()
+
+		if tail.IsEmpty() {
+			return -1
+		}
+
+		curr = tail.(*list)
+		idx++
+	}
+}
+
+func (l *list) Map(f func(interface{}) interface{}) []interface{} {
+	return append(l.tail.Map(f), f(l.head))
+}
