c71e63d7798bfdb740ba06fe9b619cc739774fd0
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-11-24 20:53:27 +0600

    add : main file
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	a := make(map[int]string)
+	a[1] = "Hello"
+	a[2] = "World"
+	a[3] = "Nawaz"
+	a[4] = "Varun"
+	a[5] = "Huma"
+
+	val, ok := a[6]
+	fmt.Println(val, ok)
 }
