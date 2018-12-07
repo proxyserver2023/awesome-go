5979909ce783cd2510653bd8a2fcb7c41dad1ffc
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-07 15:21:22 +0600

    working for rewriting mux

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/alamin-mahamud/awesome-go/pkg/mux"
+
+func main() {
+	mux.Run()
+}
---
+++ pkg/mux/app.go
@@ -0,0 +1,7 @@
+package mux
+
+import "fmt"
+
+func Run() {
+	fmt.Println("HELLO World")
+}
