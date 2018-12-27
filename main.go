d93814c0743164fbc9c0cec10d00f7f8b2da4c30
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-12-27 08:26:54 +0600

    :sparkles: cache middleware

--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/alamin-mahamud/awesome-go/pkg/cache"
+
 func main() {
+	cache.Run()
 }
---
+++ pkg/cache/cache.go
@@ -0,0 +1,7 @@
+package cache
+
+import "fmt"
+
+func Run() {
+	fmt.Println("He")
+}
