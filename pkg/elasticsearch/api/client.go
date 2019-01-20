d84a873f9ae03da8a28d0a91f94af6c2814b1e0a
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-20 17:00:23 +0600

    client for elasticsearch

--- pkg/elasticsearch/api/client.go
+++ pkg/elasticsearch/api/client.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"github.com/olivere/elastic"
+)
+
+func connectElasticSearch() *elastic.Client {
+	client, err := elastic.NewClient()
+	if err != nil {
+		// Handle Error
+	}
+	return client
+}
