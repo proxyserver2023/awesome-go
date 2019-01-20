84c5fe891aeaca4e2e7dbaeb20d4206b19224e03
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-20 17:01:05 +0600

    create index for elasticsearch

--- pkg/elasticsearch/api/createindex.go
+++ pkg/elasticsearch/api/createindex.go
@@ -1,10 +1,13 @@
 package main
 
 import "context"
+import (
+	"github.com/olivere/elastic"
+)
 
 func createIndex(client *elastic.Client, indexName string, ctx context.Context) {
 	_, err := client.CreateIndex(indexName).Do(ctx)
 	if err != nil {
-
+		// Handle ERrrror
 	}
 }
