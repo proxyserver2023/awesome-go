0210d75e191743da8d7ca1e00da4e5212fa89cc8
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-20 17:00:50 +0600

    check for specific index

--- pkg/elasticsearch/api/checkindex.go
+++ pkg/elasticsearch/api/checkindex.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"context"
+
+	"github.com/olivere/elastic"
+)
+
+func checkIndex(client *elastic.Client, indexName string, ctx context.Context) bool {
+	exists, err := client.IndexExists(indexName).Do(ctx)
+	if err != nil {
+		// Handle Error
+	}
+	return exists
+}
