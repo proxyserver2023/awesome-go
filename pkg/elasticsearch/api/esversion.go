70d606e3b63eeb23949de41520fe6b011b2b1c03
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-01-20 17:02:14 +0600

    get elasticsearch version

--- pkg/elasticsearch/api/esversion.go
+++ pkg/elasticsearch/api/esversion.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func getESVersion(client *elastic.Client, ctx context.Context) string{
+	"github.com/olivere/elastic"
+)
+
+func getESVersion(client *elastic.Client, ctx context.Context) string {
 	esversion, err := client.ElasticsearchVersion("http://127.0.0.1:9200")
 	if err != nil {
 		// Handle error
